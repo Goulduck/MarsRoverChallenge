5de745dece50c7d172fc34ca9695f9d9ba859082
Chris Goulden <goulduck@goulducks-pro.home>
2020-12-09 12:17:06 +0000

    Initial commit and test ideas

---
+++ test/commandExecuter.spec.js
@@ -0,0 +1,70 @@
+describe('Command Executer', function() {
+    it('Can split and trim input instructions', () => {
+
+    })
+
+    it('Validates instructions', () => {
+        describe('Plateau co-ordinates', function() {
+        
+        })
+
+        describe('Rover co-ordinates', function() {
+        
+        })
+
+        describe('Rover move instruction', function() {
+        
+        })
+
+    })
+
+    describe('Plots out the plateau', () => {
+        describe('for valid co-ordinates', () => {
+            it('Returns Plateau', function() {
+            
+            })
+        })
+
+        describe('for invalid co-ordinates', () => {
+            it('Returns Error', function() {
+            
+            })
+        })
+
+        describe('for negative co-ordinates', () => {
+            it('Returns Error', function() {
+            
+            })
+        })
+    })
+
+    describe('Creates a rover', () => {
+        describe('with valid co-ordinates', () => {
+            it('Returns Rover', function() {
+            
+            })
+        })
+
+        describe('when not on plateau', () => {
+            it('Returns Error', function() {
+            
+            })
+        })
+
+        describe('for invalid co-ordinates', () => {
+            it('Returns Error', function() {
+            
+            })
+        })
+
+        describe('with invalid move instruction', () => {
+            it('Returns Error', function() {
+            
+            })
+        })
+    })
+
+
+
+
+})
---
+++ test/marsRover.spec.js
@@ -0,0 +1,75 @@
+describe("MarsRover", function() {
+    it("Returns correct co-ordinates", () => {
+
+    })
+
+    it("Rover coordinates must be integer", () => {
+        
+    })
+
+    it("Rover coordinates must on the plateau", () => {
+        
+    })
+
+    it("Rover must be facing a valid direction", () => {
+        
+    })
+
+    describe('Moves in the correct direction', ()=> {
+        it("Moves North", () => {
+        
+        })
+
+        it("Moves East", () => {
+        
+        })
+
+        it("Moves South", () => {
+        
+        })
+
+        it("Moves West", () => {
+        
+        })
+    })
+
+    describe('Turns the correct direction', ()=> {
+        describe('Turns Left', ()=> {
+            it("When facing North", () => {
+            
+            })
+    
+            it("When facing East", () => {
+            
+            })
+    
+            it("When facing South", () => {
+            
+            })
+    
+            it("When facing West", () => {
+            
+            })
+        })
+
+        describe('Turns Right', ()=> {
+            it("When facing North", () => {
+            
+            })
+    
+            it("When facing East", () => {
+            
+            })
+    
+            it("When facing South", () => {
+            
+            })
+    
+            it("When facing West", () => {
+            
+            })
+        })
+    })
+
+
+})
